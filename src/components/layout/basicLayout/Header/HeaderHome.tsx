943369f81220e18849a858c2753f98c84dec821e
bahachammakhi <bahachammakhi25@gmail.com>
2020-02-29 18:25:30 +0100

    Added header

--- src/components/layout/basicLayout/Header/HeaderHome.tsx
+++ src/components/layout/basicLayout/Header/HeaderHome.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Layout, Menu, Breadcrumb } from 'antd';
 import classes from './header.module.scss';
 import logo from '../../../../assets/logo.png';
 const { Header, Content, Footer } = Layout;
+const menus = ['home', 'aboutus', 'sevices', 'causes', 'contact'];
 
 const HeaderHome = () => {
+  const [selecteditem, setSelectedItem] = useState(0);
+  const handleClicked = (props: any) => {
+    setSelectedItem(props.key);
+  };
+  const menusItems = menus.map((element, index) => {
+    return (
+      <Menu.Item className={selecteditem === index ? classes.selecteditem : classes.navmenuitem} key={index}>
+        {element}
+      </Menu.Item>
+    );
+  });
   return (
     <Header className={classes.navbar}>
       <div className={classes.logo}>
-        <img src={logo} className={classes.logoimg} alt="logo" />
+        {/* <img src={logo} className={classes.logoimg} alt="logo" /> */}
+        <h1 className={classes.logotitle}>Hope-APP</h1>
       </div>
-      <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+      <Menu
+        className={classes.menu}
+        onClick={props => handleClicked(props)}
+        theme="light"
+        mode="horizontal"
+        defaultSelectedKeys={[selecteditem.toString()]}
+        style={{ lineHeight: '64px' }}
+      >
+        {menusItems}
       </Menu>
     </Header>
   );
