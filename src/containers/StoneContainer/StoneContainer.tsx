35a34f91c43d48659ed5ce602957b0fe2a43a140
bahachammakhi <bahachammakhi25@gmail.com>
2020-03-19 03:47:40 +0100

    Working on form creator

--- src/containers/StoneContainer/StoneContainer.tsx
+++ src/containers/StoneContainer/StoneContainer.tsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import useApi from '../../hooks/useApi';
 import { getStones } from '../../requests';
 import StoneGrid from '../../components/ui/stonegrid/StoneGrid';
+import useFormCreator from '../../hooks/UseFormCreator';
+import formCreator from '../../utils/formCreator';
+import { Form, Button } from 'antd';
+import useForm from '../../hooks/useForm';
+
 interface stones {
   startDate: Date;
   name: string;
@@ -33,9 +38,46 @@ interface stones {
 }
 
 export const StoneContainer = () => {
+  const initialState = {
+    input: '',
+    password: '',
+  };
+  const { handleChange, form, handleReset, selectform, handleSelect } = useForm({ initialState });
   const [data, setData] = useState<stones[]>([]);
+  const forms = [
+    {
+      elementType: 'input',
+      elementConfig: {
+        label: 'Email',
+        name: 'input',
+        size: 'default',
+        placeholder: 'test',
+      },
+    },
+    {
+      elementType: 'password',
+      elementConfig: {
+        label: 'Password',
+        name: 'password',
+        placeholder: 'test',
+      },
+    },
+    {
+      elementType: 'select',
+      elementConfig: {
+        label: 'Select',
+        name: 'select',
+        placeholder: 'test',
+        options: [
+          { name: 'Choose1', value: '2' },
+          { name: 'Choose3', value: '3' },
+        ],
+      },
+    },
+  ];
+
+  const { renderForms } = useFormCreator({ forms, handleChange, handleSelect });
   const { ...calls } = useApi({ getStones });
-  console.log(calls.getStones.data.data?.stones);
 
   useEffect(() => {
     calls.getStones.call();
@@ -43,6 +85,30 @@ export const StoneContainer = () => {
   useEffect(() => {
     if (calls.getStones.data.data?.stones) setData(calls.getStones.data.data?.stones);
   }, [calls.getStones.success]);
+  console.log('initial', form, 'select', selectform);
+  return (
+    <div>
+      {data ? <StoneGrid data={data} /> : <h1>No stones yet</h1>}
 
-  return <div>{data ? <StoneGrid data={data} /> : <h1>No stones yet</h1>}</div>;
+      {/* {formCreator({
+        elementType: 'password',
+        elementConfig: {
+          label: 'test',
+          name: 'name',
+        },
+        onChange: () => {
+          console.log('test');
+        },
+        value: 'Now',
+      })} */}
+      <Form name="time_related_controls" labelCol={{ span: 5 }} wrapperCol={{ span: 5 }} layout="horizontal">
+        {renderForms}
+        <Form.Item wrapperCol={{ span: 8, offset: 5 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
