73ab812f4fc0ae7bd51b7ecde45c79496d93512d
hakimmaaouia <hakimmaaouia@gmail.com>
2020-03-23 21:54:05 +0100

    validation

--- src/containers/Donatepage/Formadd.tsx
+++ src/containers/Donatepage/Formadd.tsx
@@ -10,71 +10,48 @@ const { Title } = Typography;
 const { Text } = Typography;
 const { TextArea } = Input;
 
- 
+const Formadd = () => {
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 16 },
+  };
+  return (
+    <div>
+      <Title>Form</Title>
+      <Form {...layout}>
+        <Form.Item label="Username" name="username">
+          <Input placeholder="Basic usage" />
+        </Form.Item>
 
-const Formadd = ()=> {
+        <Form.Item label="title" name="title">
+          <Input placeholder="Basic usage" />
+        </Form.Item>
 
-    const layout = {
-        labelCol: { span: 6 },
-        wrapperCol: { span: 16 },
-      };
-    return(
-      <div>   
-          <Title>Form</Title>
-<Form   {...layout}>
-<Form.Item
-        label="Username"
-        name="username"
-         >
-            <Input placeholder="Basic usage" />
-            </Form.Item>
+        <Form.Item label="description" name="description">
+          <TextArea placeholder="description" allowClear />
+        </Form.Item>
 
-            <Form.Item
-        label="title"
-        name="title"
-         >
-            <Input placeholder="Basic usage" />
-            </Form.Item>
+        <Form.Item label="contact" name="contact">
+          <Input placeholder="contact" />
+        </Form.Item>
+        <Form.Item label="cover image" name="cover image">
+          <Upload>
+            <Button>
+              <UploadOutlined /> Click to Upload
+            </Button>
+          </Upload>
+        </Form.Item>
 
-            <Form.Item
-        label="description"
-        name="description"
-      >
-            <TextArea placeholder="description" allowClear  />
-            </Form.Item>
-           
-            <Form.Item
-        label="contact"
-        name="contact"
-         >
-            <Input placeholder="contact" />
-            </Form.Item>
-            <Form.Item
-        label="cover image"
-        name="cover image"
-         >
-          <Upload>        
-    <Button>
-      <UploadOutlined /> Click to Upload
-    </Button>
-  </Upload>
-  
- </Form.Item>
+        <Form.Item label="image" name="image">
+          <Upload>
+            <Button>
+              <UploadOutlined /> Click to Upload
+            </Button>
+          </Upload>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-            <Form.Item
-        label="image"
-        name="image"
-         >
-          <Upload>        
-    <Button>
-      <UploadOutlined /> Click to Upload
-    </Button>
-  </Upload>
-  
- </Form.Item>
- </Form>
-      </div>
-    );
-  }
-  
-export default Formadd  
+export default Formadd;
